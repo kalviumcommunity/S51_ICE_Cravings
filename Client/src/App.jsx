66eb1837a198137715e8db30a2a630e44d2651e4
Manuel Beracah <manuel.b@kalvium.community>
2024-02-28 22:27:24 +0530

    Update src

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <h1>It's an Ice Cravings app, you can find here various and vast types of Ice.</h1>
+      <h1>It's an Ice Cravings app, you can find here various and vast types of Ice. </h1>
     </>
   )
 }
