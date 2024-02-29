f6630e975eac682c3f062a2e13fa3869880f5ecf
Manuelberacah <141898668+Manuelberacah@users.noreply.github.com>
2024-02-29 11:01:57 +0530

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,9 +3,9 @@ import './App.css'
 function App() {
 
   return (
-    <>
+    <div>
       <h1>It's an Ice Cravings app, you can find here various and vast types of Ice.</h1>
-    </>
+    </div>
   )
 }
 
